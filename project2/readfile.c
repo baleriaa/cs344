69fc2a1cd923cd422d42ccfe4d68187234a2410f
Baleria Reyes <breyes815@gmail.com>
2023-01-22 08:55:34 -0800

    readfile working okay.

---
+++ project2/readfile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])  {
+  int file_descriptor =  open(argv[1], 'O_RDONLY');
+  char buffer[2048];
+
+  if (file_descriptor == -1) {
+    perror("error opening file");
+  }
+
+  int bytes_read = read(file_descriptor, buffer, sizeof(buffer));
+  if (bytes_read == -1) {
+    perror("error reading file");
+  }
+  
+  write(1, buffer, bytes_read);
+
+}
