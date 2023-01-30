bba381f0cc936c72579f0ae66fb07f4cbff375ca
Baleria Reyes <breyes815@gmail.com>
2023-01-29 21:55:45 -0800

    part 1 working.

--- project3/mush.c
+++ project3/mush.c
@@ -1,9 +1,26 @@
+// my own shell program
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[]){
+int main(void){
+  char prompt[] = "mush> ";
+  char *token;
+  char buffer[2048];
+  char *words[128];
+  int i = 0;
   
+  printf("%s", prompt);
+  fgets(buffer, sizeof buffer, stdin);
+  if ((token = strtok(buffer, " \t\r\n")) != NULL) {
+    do {
+      printf("%s\n", token);
+      words[i++] = token;
+    } while ((token = strtok(NULL, " \t\r\n")) != NULL);
+  }
+  words[i++] = NULL;
+  execvp(words[0], words);
 }
