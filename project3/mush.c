03c1fe83195a1121d4b40490f6f87e0607f4ffc4
Baleria Reyes <breyes815@gmail.com>
2023-02-01 17:28:25 -0800

    mostly working except for cd not changing directories.

--- project3/mush.c
+++ project3/mush.c
@@ -14,13 +14,30 @@ int main(void){
   int i = 0;
   
   printf("%s", prompt);
+  fflush(stdout);
   fgets(buffer, sizeof buffer, stdin);
   if ((token = strtok(buffer, " \t\r\n")) != NULL) {
     do {
-      printf("%s\n", token);
-      words[i++] = token;
-    } while ((token = strtok(NULL, " \t\r\n")) != NULL);
+      words[i] = token;
+      i++;
+    } 
+    while ((token = strtok(NULL, " \t\r\n")) != NULL);
   }
-  words[i++] = NULL;
-  execvp(words[0], words);
+  words[i] = NULL;
+
+  if (strcmp(words[0], "cd") == 0) {
+    if (chdir(words[1]) != 0) {
+      perror("chdir error");
+    }
+  }
+
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    execvp(words[0], words);
+    exit(0);
+  } else {
+    wait(NULL);
+  }
+  perror("execvp");
 }
