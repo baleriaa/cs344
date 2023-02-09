280c5a548d08dccf0a0026397baccf8ce863a118
Baleria Reyes <breyes815@gmail.com>
2023-02-08 19:57:53 -0800

    remove comments.

--- project4/lswc.c
+++ project4/lswc.c
@@ -1,15 +1,3 @@
-// Parse the command line
-// Make a pipe() in the parent
-// fork().
-// The child will run wc:
-// dup2() the read end of the pipe into stdin
-// close() the write end of the pipe
-// execlp() the wc command
-// The parent will run ls:
-// dup2() the write end of the pipe into stdout
-// close() the read end of the pipe
-// execlp() the ls command
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
