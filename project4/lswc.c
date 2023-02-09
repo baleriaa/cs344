23bb4deb059cfbd2ff697de3fd1639830be6b707
Baleria Reyes <breyes815@gmail.com>
2023-02-08 19:56:52 -0800

    lswc working.

--- project4/lswc.c
+++ project4/lswc.c
@@ -15,15 +15,22 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    (void) argc;
     int pfd[2];
-    int process_id = fork();
-
     pipe(pfd);
+    pid_t process_id = fork();
 
-    if (process_id == 0) {
-        // Child process
-    } else {
-        // Parent process
+    if (process_id == 0) { // child
+        dup2(pfd[0], STDIN_FILENO); // read end of pipe into stdin
+        close(pfd[1]);
+        execlp("wc", "wc", "-l", NULL);
+    } else {    // parent
+        dup2(pfd[1], STDOUT_FILENO); // write end of pipe into stdout
+        close(pfd[0]);
+        if (argc > 1) {
+            execlp("ls", "ls", "-1a", argv[1], NULL);
+        } else if (argc == 1) {
+            execlp("ls", "ls", "-1a", NULL);
+        }
     }
-
 }
