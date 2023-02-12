2e067b891839cd410e8db50b1d02db549bde6d03
Baleria Reyes <breyes815@gmail.com>
2023-02-12 10:49:16 -0800

    testing functions so far. Mostly working except for printing when empty.

--- project5/llist.c
+++ project5/llist.c
@@ -4,47 +4,67 @@
 #include "llist.h"
 
 int main (int argc, char *argv[]) {
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "ih") == 0) {
-            print("%s", argv[i]);
-            // llist_insert_head();
-        } 
-        else if (strcmp(argv[i], "it") == 0) {
-            // llist_insert_tail();
-            print("%s", argv[i]);
-        } 
-        else if (strcmp(argv[i], "dh") == 0) {
-            // llist_delete_head();
-            print("%s", argv[i]);
-        }
-        else if (strcmp(argv[i], "f") == 0) {
-            // llist_free();
-            print("%s", argv[i]);
-        }
-        else if (strcmp(argv[i], "p") == 0) {
-            // llist_print();
-            print("%s", argv[i]);
-        }
-        else {
-            print("Invalid command. Please choose either: ih, it, dh, f, or p.");
-        }
-    }
+    // for (int i = 1; i < argc; i++) {
+    //     if (strcmp(argv[i], "ih") == 0) {
+    //         printf("%s", argv[i]);
+    //         // llist_insert_head();
+    //     } 
+    //     else if (strcmp(argv[i], "it") == 0) {
+    //         // llist_insert_tail();
+    //         printf("%s", argv[i]);
+    //     } 
+    //     else if (strcmp(argv[i], "dh") == 0) {
+    //         // llist_delete_head();
+    //         printf("%s", argv[i]);
+    //     }
+    //     else if (strcmp(argv[i], "f") == 0) {
+    //         // llist_free();
+    //         printf("%s", argv[i]);
+    //     }
+    //     else if (strcmp(argv[i], "p") == 0) {
+    //         // llist_print();
+    //         printf("%s", argv[i]);
+    //     }
+    //     else {
+    //         printf("Invalid command. Please choose either: ih, it, dh, f, or p.");
+    //         exit(1);
+    //     }
+    // }
+    struct node *test_node = node_alloc(45);
+    llist_print(test_node);
+    node_free(test_node);
+    llist_print(test_node);
+
 }
 
 struct node *node_alloc(int value) {
     struct node *node = malloc(sizeof(struct node));
     node->value = value;
     node->next = NULL;
+    return node;
 }
 
 void node_free(struct node *n) {
     free(n);
 }
 
+void llist_print(struct node *head) {
+    struct node *current = head;
+    if (current != NULL) {
+        while (current != NULL) {
+            printf("%d ->", current->value);
+            current = current->next;
+        }
+    }
+    else {
+        printf("[empty]");
+    }    
+}
+
 void llist_insert_head(struct node **head, struct node *n);
 struct node *llist_delete_head(struct node **head);
 void llist_insert_tail(struct node **head, struct node *n);
-void llist_print(struct node *head);
+
 void llist_free(struct node **head);
 
 
