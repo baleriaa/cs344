ee0e2940237434511201638422ccf375f55dd2dc
Baleria Reyes <breyes815@gmail.com>
2023-03-22 09:55:32 -0700

    new_process ok excluding error handling.

--- project9/ptsim.c
+++ project9/ptsim.c
@@ -47,29 +47,29 @@ int allocate_page() {
             mem[i] = 1;
             return i;
         }
-        return 0xff;
+
     }
+    return 0xff;
 }
 
 void new_process(int proc_num, int page_count)
 {
-    // // Get the page table page
-    // page_table = AllocatePage()
+//     If the initial page table allocation fails (due to out-of-memory), the function should print:
 
-    // // Set this process's page table pointer in zero page
-    // mem[64 + proc_num] = page_table
+//     printf("OOM: proc %d: page table\n", proc_num);
+// and return.
 
-    // // Allocate data pages
-    // For i from 0 to page_count:
-    //     new_page = AllocatePage()
+// If any of the subsequent page allocations fail, it should print:
 
-    //     // Set the page table to map virt -> phys
-    //     // Virtual page number is i
-    //     // Physical page number is new_page
-    //     pt_addr = GetAddress(page_table, i)
-    //     mem[pt_addr] = new_page
+//     printf("OOM: proc %d: data page\n", proc_num);
+    int page_table = allocate_page();
+    mem[64 + proc_num] = page_table;
     
-     
+    for (int i = 0; i < page_count; i++) {
+        int new_page = allocate_page();
+        int pt_addr = get_address(page_table, i);
+        mem[pt_addr] = new_page;
+    } 
 }
 
 //
